fe9f95a78e6b3b4d30b6a7bf32492a0eb4a1c640
Lemon <99694629+eplemonsqueezys@users.noreply.github.com>
2024-07-07 21:57:35 -0400

    fixed stuff and gates

--- code.ino
+++ code.ino
@@ -7,6 +7,8 @@ int sawswitchPin = 5; // Pin the saw switch is connected to
 int sweepswitchPin = 4; // Pin the sweep switch is connected to
 const int dustCollectionRelayPin = 7; // Dust collection relay pin
 const int tablesawRelayPin = 3; // Table saw relay pin
+int high = 127;
+int low = 55;
 
 int sawswitchState; // The state of the saw switch
 int sweepswitchState; // The state of the sweep switch
@@ -25,9 +27,11 @@ void setup() {
   digitalWrite(tablesawRelayPin, LOW); // Initialize table saw relay pin to LOW
   Serial.begin(9600); // Start serial communication at 9600 baud rate
   digitalWrite(dustCollectionRelayPin, HIGH); // Set dust collection relay pin to HIGH
-  sweepservo.write(90); // moves the sweep servo to closed position
-  delay(15); // waits 15 milisecounds to give the servo time to move to the position
-  sawservo.write(90); // move the saw servo to closed position
+  sweepservo.write(high);
+  sawservo.write(high);
+  sweepservo.write(low); // moves the sweep servo to closed position
+  sawservo.write(low);
+  delay(15); // waits 15 milisecounds to give the servo time to move to the position // move the saw servo to closed position
   delay(5000); // Wait for 5 seconds
 }
 
@@ -36,17 +40,16 @@ void loop() {
 
   sweepswitchState = digitalRead(sweepswitchPin); // Read the state of the sweep switch
   sawswitchState = digitalRead(sawswitchPin); // Read the state of the saw switch
-
   if (sweepswitchState == LOW) { // If sweep switch is pressed
     Serial.println("Sweep Switch ON"); // Print message to serial
-    sweepservo.write(120); // Move sweepservo to 120 degrees
+    sweepservo.write(high); // Move sweepservo to 120 degrees
     if (!collectorIsOn) { // If the dust collector is not on
       Serial.println("turnOnDustCollection"); // Print message to serial
       digitalWrite(dustCollectionRelayPin, LOW); // Turn on dust collection relay
       collectorIsOn = true; // Update collector state to on
     }
   } else { // If sweep switch is not pressed
-    sweepservo.write(90); // Move sweepservo to 90 degrees
+    sweepservo.write(low); // Move sweepservo to 90 degrees
     if (collectorIsOn && sawswitchState != LOW) { // If collector is on and saw switch is not pressed
       Serial.println("Sweep Switch OFF"); // Print message to serial
       Serial.println("turnOffDustCollection"); // Print message to serial
@@ -57,7 +60,7 @@ void loop() {
 
   if (sawswitchState == LOW) { // If saw switch is pressed
     Serial.println("Saw Switch ON"); // Print message to serial
-    sawservo.write(120); // Move sawservo to 120 degrees
+    sawservo.write(high); // Move sawservo to 120 degrees
     digitalWrite(tablesawRelayPin, LOW); // Turn on table saw relay
     delay(15); // Wait for 15ms for servo to move
     if (!collectorIsOn) { // If the dust collector is not on
@@ -68,7 +71,7 @@ void loop() {
       delay(15); // Wait for 15ms for relay to activate
     }
   } else { // If saw switch is not pressed
-    sawservo.write(90); // Move sawservo to 90 degrees
+    sawservo.write(low); // Move sawservo to 90 degrees
     digitalWrite(tablesawRelayPin, HIGH); // Turn off table saw relay
     delay(15); // Wait for 15ms for servo to move
     if (collectorIsOn && sweepswitchState != LOW) { // If collector is on and sweep switch is not pressed
